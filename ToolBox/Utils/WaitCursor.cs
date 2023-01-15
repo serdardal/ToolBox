803a850702e5cc2b753d47992190504a6a8b1525
Serdar Dal <serdardal9807@gmail.com>
2023-01-15 23:44:34 +0300

    cleanup

--- ToolBox/Utils/WaitCursor.cs
+++ ToolBox/Utils/WaitCursor.cs
@@ -4,25 +4,23 @@
     {
         private static List<WaitCursorSubscriber> Subscribers { get; } = new List<WaitCursorSubscriber>();
 
-        private static void UnSubscribe(WaitCursorSubscriber subscriber) {
-            Subscribers.Remove(subscriber);
-            if(!Subscribers.Any())
-            {
-                Application.UseWaitCursor = false;
-            }
-        }
-
         public static WaitCursorSubscriber Subscribe()
         {
             if (!Subscribers.Any())
-            {
                 Application.UseWaitCursor = true;
-            }
 
             var newSubscriber = new WaitCursorSubscriber();
             Subscribers.Add(newSubscriber);
 
             return newSubscriber;
+        }
+
+        private static void UnSubscribe(WaitCursorSubscriber subscriber)
+        {
+            Subscribers.Remove(subscriber);
+
+            if (!Subscribers.Any())
+                Application.UseWaitCursor = false;
         }
 
         public class WaitCursorSubscriber : IDisposable
